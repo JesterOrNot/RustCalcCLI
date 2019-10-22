3dfbef0fb2fd1ef156eaeeb9bb38cfc3038d16f4
Sean Hellum <seanhellum45@gmail.com>
2019-10-22 17:51:12 +0000

    add the `add` function

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ fn main() {
         .version("0.1")
         .author("Sean Hellum")
         .about("Hello world cli")
-        .args_from_usage("-r --double 'Say hello'");
+        .args_from_usage("-r --double 'Say hello'
+                         -a --add 'add 2 numbers'");
 
     let matches = app.get_matches();
     if matches.is_present("double") {
         double_me();
+    }
+    if matches.is_present("add") {
+        addNums();
     }
 }
 fn double_me() {
@@ -20,4 +24,17 @@ fn double_me() {
     std::io::stdin().read_line(&mut line).unwrap();
     let n: i32 = line.trim().parse().unwrap();
     println!("{} doubled is {}",n,2*n);
+}
+fn addNums() {
+    print!("What is the first number?: ");
+    io::stdout().flush().unwrap();
+    let mut item1 = String::new();
+    std::io::stdin().read_line(&mut item1).unwrap();
+    let item1: i32 = item1.trim().parse().unwrap();
+    print!("What is the second number?: ");
+    io::stdout().flush().unwrap();
+    let mut item2 = String::new();
+    std::io::stdin().read_line(&mut item2).unwrap();
+    let item2: i32 = item2.trim().parse().unwrap();
+    println!("The sum is {}",item1+item2);
 }
